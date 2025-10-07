5bd4e84c1ee04c9ad94a04f66beae16cfb544992
Ahmed <162884965+theahmed619@users.noreply.github.com>
2025-10-07 21:05:10 +0530

    Update settings.py

--- devchat/devchat/settings.py
+++ devchat/devchat/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.accounts',
-    'apps.chat',
+    'devchat.apps.accounts',
+    'devchat.apps.chat',
     
 ]
 
