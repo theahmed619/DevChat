d419fd56394e9d3eed55406272886523b59e0b85
Ahmed <162884965+theahmed619@users.noreply.github.com>
2025-10-07 21:28:26 +0530

    Update settings.py

--- devchat/devchat/settings.py
+++ devchat/devchat/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'devchat.devchat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
